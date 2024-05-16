0f1e298f925df3b29bcc1b6895d75ee317b28326
Bruna Lucas <151275054+BrunaLucad2004@users.noreply.github.com>
2024-05-16 10:18:54 -0300

    Update matriz.go

--- matriz.go
+++ matriz.go
@@ -6,6 +6,23 @@ import (
 	"time"
 )
 
+func MultiplyRow(row []float64, scalar float64) {
+
+	//As funcoes para tempo e ver as goroutines foram retiradas dos exemplos do professor
+
+	fmt.Printf("M.Work started at %s\n", time.Now().Format("15:04:05")) //inicia a contagem do tempo
+	fmt.Println("gorotines: ", runtime.NumGoroutine())                  //informa o numero de gorotines sendo executadas
+	time.Sleep(1 * time.Second)                                         //tempo para cada processo
+
+	//multplica o escalar por cada elemento da linha
+	for i := 0; i < len(row); i++ {
+		row[i] *= scalar
+	}
+
+	fmt.Printf("Work  finished at %s\n", time.Now().Format("15:04:05")) //finaliza a contagem do tempo
+
+}
+
 func main() {
 
 	// Definir uma matriz 13x13
@@ -26,24 +43,15 @@ func main() {
 	}
 
 	// Escalar
-	k := 2.00
+	k := 28.03
 
 	// Multiplicar a matriz pelo escalar
 	for i := 0; i < 13; i++ {
-		for j := 0; j < 13; j++ {
-
-			fmt.Printf("M.Work started at %s\n", time.Now().Format("15:04:05"))
-			fmt.Println("gorotines: ", runtime.NumGoroutine())
-			time.Sleep(1 * time.Second)
-
-			matrix[i][j] *= k
-
-			fmt.Printf("Work  finished at %s\n", time.Now().Format("15:04:05"))
-		}
+		MultiplyRow(matrix[i][:], k)
 	}
 
 	// Exibir a matriz
-	fmt.Println("\n", "   Matriz resultante:")
+	fmt.Println("Matrix:")
 	for i := 0; i < 13; i++ {
 		fmt.Println(matrix[i])
 	}
